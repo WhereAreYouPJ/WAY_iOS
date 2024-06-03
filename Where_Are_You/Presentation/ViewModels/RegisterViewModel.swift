0c9113f123a48ebcb16dabf892944a9d707ea981
jungseok-corine <nonehour@naver.com>
2024-06-03 15:46:05 +0900

    ADD: LoginViewModel
    로그인 뷰모델 로직 구현

--- Where_Are_You/Presentation/ViewModels/RegisterViewModel.swift
+++ Where_Are_You/Presentation/ViewModels/RegisterViewModel.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 
+class RegisterViewModel {
+    private let userUseCase: UserUseCase
+    
+    var user: User?
+    var error: String?
+    
+    init(userUseCase: UserUseCase) {
+        self.userUseCase = userUseCase
+    }
+    
+    func register(user: User, completion: @escaping () -> Void) {
+        userUseCase.register(user: user) { [weak self] result in
+            switch result {
+            case .success(let user):
+                self?.user = user
+            case .failure(let error):
+                self?.error = error.localizedDescription
+            }
+            completion()
+        }
+    }
+}
