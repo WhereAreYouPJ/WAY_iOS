e425c245e18627a63c16956d7fef1c7ac0e313d9
jungseok-corine <nonehour@naver.com>
2024-07-15 20:12:00 +0900

    add schedule viewmodel

---
+++ Where_Are_You/Presentation/Main/ViewModels/ScheduleViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  ScheduleViewModel.swift
+//  Where_Are_You
+//
+//  Created by 오정석 on 15/7/2024.
+//
+
+import Foundation
+
+class ScheduleViewModel {
+    var onScheduleDataFetched: (() -> Void)?
+    private var schedules: [String] = []
+    private var timer: Timer?
+    private var currentIndex = 0
+    
+    func fetchSchedules() {
+        // Fetch schedules from the API
+        // Update schedules array
+        self.schedules = [
+            "D - 369 96조 여의도 한강공원 모임",
+            "D - 100 96조 워크숍"
+        ]
+        onScheduleDataFetched?()
+        startAutoScroll()
+    }
+    
+    func getSchedules() -> [String] {
+        return schedules
+    }
+    
+    func startAutoScroll() {
+        stopAutoScroll()
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(scrollToNextSchedule), userInfo: nil, repeats: true)
+    }
+    
+    func stopAutoScroll() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @objc private func scrollToNextSchedule() {
+        // Logic to automatically scroll to the next schedule
+    }
+}
