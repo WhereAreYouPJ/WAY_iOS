13dab6bbec0c560f714f04521bf1c8ab8e7d1e76
jungseok-corine <nonehour@naver.com>
2024-07-30 11:49:13 +0900

    profile view added

---
+++ Where_Are_You/Presentation/Main/FriendFeed/Views/Friends/ProfileView.swift
@@ -0,0 +1,51 @@
+//
+//  ProfileView.swift
+//  Where_Are_You
+//
+//  Created by 오정석 on 30/7/2024.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileView: UIView {
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 18
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "exampleProfileImage")
+        return imageView
+    }()
+    
+    let userNameLabel = CustomLabel(UILabel_NotoSans: .medium, text: "유민혁", textColor: .color34, fontSize: 14)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureViewComponents()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureViewComponents() {
+        addSubview(profileImageView)
+        addSubview(userNameLabel)
+    }
+    
+    private func setupConstraints() {
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(12)
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().inset(20)
+            make.width.height.equalTo(self)
+        }
+        
+        userNameLabel.snp.makeConstraints { make in
+            make.left.equalTo(profileImageView.snp.right).offset(6)
+            make.centerY.equalToSuperview()
+        }
+    }
+}
