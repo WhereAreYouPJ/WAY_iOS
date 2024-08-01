a3df04156847ad1bce2147f106a2a7beb69f6a3e
Juhee <viimv2000@gmail.com>
2024-08-01 20:29:12 +0900

    feat: 일정 추가 파일 생성

---
+++ Where_Are_You/Presentation/Login/Login/CreateScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateScheduleView.swift
+//  Where_Are_You
+//
+//  Created by juhee on 01.08.24.
+//
+
+import SwiftUI
+
+struct CreateScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateScheduleView()
+}
---
+++ Where_Are_You/Utils/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleView.swift
+//  Where_Are_You
+//
+//  Created by juhee on 01.08.24.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
