e9a278321ce7edf36fd5df418087e87bb223d6b3
jungseok-corine <nonehour@naver.com>
2024-08-27 10:32:58 +0900

    feat(#7): 마이페이지 내정보 관리 뷰 추가
    마이페이지-내정보관리 뷰 추가

---
+++ Where_Are_You/Presentation/Main/Mypage/ViewControllers/MyDetailManageViewcontroller.swift
@@ -0,0 +1,8 @@
+//
+//  MyDetailManageViewcontroller.swift
+//  Where_Are_You
+//
+//  Created by 오정석 on 23/8/2024.
+//
+
+import Foundation
---
+++ Where_Are_You/Presentation/Main/Mypage/Views/MyDetailManageView.swift
@@ -0,0 +1,8 @@
+//
+//  MyDetailManageView.swift
+//  Where_Are_You
+//
+//  Created by 오정석 on 23/8/2024.
+//
+
+import Foundation
