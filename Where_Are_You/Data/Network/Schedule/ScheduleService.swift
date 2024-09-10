8538da57a841c9965640b8e83256046dc7b1f943
jungseok-corine <nonehour@naver.com>
2024-09-10 15:56:26 +0700

    feat(#23): 홈화면 DDay일정 네트워크 완성

---
+++ Where_Are_You/Data/Network/Schedule/ScheduleService.swift
@@ -0,0 +1,88 @@
+//
+//  ScheduleService.swift
+//  Where_Are_You
+//
+//  Created by 오정석 on 10/9/2024.
+//
+
+import Alamofire
+import Moya
+
+// MARK: - ScheduleServiceProtocol
+
+protocol ScheduleServiceProtocol {
+    func postSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, Error>) -> Void)
+    func getSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, Error>) -> Void)
+    func putSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, Error>) -> Void)
+    func deleteSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, Error>) -> Void)
+    func postEcceptSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, Error>) -> Void)
+    func getMonthSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, Error>) -> Void)
+    func getDate(request: CreateScheduleBody, completion: @escaping (Result<Void, Error>) -> Void)
+    func getDDaySchedule(completion: @escaping (Result<GenericResponse<DDayScheduleResponse>, Error>) -> Void)
+}
+
+// MARK: - ScheduleService
+
+class ScheduleService: ScheduleServiceProtocol {
+    private var provider = MoyaProvider<ScheduleAPI>()
+    
+    private var memberSeq: Int {
+        return UserDefaultsManager.shared.getMemberSeq()
+    }
+    
+    init() {
+        let tokenPlugin = AuthTokenPlugin(tokenClosure: {
+            return UserDefaultsManager.shared.getAccessToken()
+        })
+        self.provider = MoyaProvider<ScheduleAPI>(plugins: [tokenPlugin])
+    }
+    
+    // MARK: - APIService
+    func postSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, any Error>) -> Void) {
+        provider.request(.postSchedule(request: request)) { result in
+            APIResponseHandler.handleResponse(result, completion: completion)
+        }
+    }
+    
+    func getSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, any Error>) -> Void) {
+        provider.request(.getSchedule(request: request)) { result in
+            APIResponseHandler.handleResponse(result, completion: completion)
+        }
+    }
+    
+    func putSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, any Error>) -> Void) {
+        provider.request(.putSchedule(request: request)) { result in
+            APIResponseHandler.handleResponse(result, completion: completion)
+        }
+    }
+    
+    func deleteSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, any Error>) -> Void) {
+        provider.request(.deleteSchedule(request: request)) { result in
+            APIResponseHandler.handleResponse(result, completion: completion)
+        }
+    }
+    
+    func postEcceptSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, any Error>) -> Void) {
+        provider.request(.postEcceptSchedule(request: request)) { result in
+            APIResponseHandler.handleResponse(result, completion: completion)
+        }
+    }
+    
+    func getMonthSchedule(request: CreateScheduleBody, completion: @escaping (Result<Void, any Error>) -> Void) {
+        provider.request(.getMonthSchedule(request: request)) { result in
+            APIResponseHandler.handleResponse(result, completion: completion)
+        }
+    }
+    
+    func getDate(request: CreateScheduleBody, completion: @escaping (Result<Void, any Error>) -> Void) {
+        provider.request(.getDate(request: request)) { result in
+            APIResponseHandler.handleResponse(result, completion: completion)
+        }
+    }
+    
+    func getDDaySchedule(completion: @escaping (Result<GenericResponse<DDayScheduleResponse>, Error>) -> Void) {
+        provider.request(.getDDaySchedule(memberSeq: memberSeq)) { result in
+            APIResponseHandler.handleResponse(result, completion: completion)
+        }
+    }
+}
---
+++ Where_Are_You/Utils/APIResponseHandler.swift
@@ -0,0 +1,44 @@
+//
+//  APIResponseHandler.swift
+//  Where_Are_You
+//
+//  Created by 오정석 on 10/9/2024.
+//
+
+import Foundation
+import Moya
+
+class APIResponseHandler {
+    static func handleResponse<T>(_ result: Result<Moya.Response, MoyaError>, completion: @escaping (Result<T, Error>) -> Void) where T: Decodable {
+        switch result {
+        case .success(let response):
+            do {
+                // TODO: 앱 개발 완료후 지우기(받는 데이터 정보 확인용)
+                if let json = try? response.mapJSON() {
+                    print("Response JSON: \(json)")
+                }
+                
+                let data = try response.map(T.self)
+                completion(.success(data))
+            } catch let error {
+                completion(.failure(error))
+            }
+        case .failure(let error):
+            completion(.failure(error))
+        }
+    }
+    
+    static func handleResponse(_ result: Result<Moya.Response, MoyaError>, completion: @escaping (Result<Void, Error>) -> Void) {
+        switch result {
+        case .success(let response):
+            do {
+                _ = try response.filterSuccessfulStatusCodes()
+                completion(.success(()))
+            } catch let error {
+                completion(.failure(error))
+            }
+        case .failure(let error):
+            completion(.failure(error))
+        }
+    }
+}
