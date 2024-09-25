03c1e4fbfa15339a8272996bc80b3e9ec9301934
jungseok-corine <nonehour@naver.com>
2024-09-25 17:24:04 +0900

    feat(#6): 새피드작성
    새피드작성 - 일정 선택 구현

--- Where_Are_You/Domain/Entities/ScheduleList.swift
+++ Where_Are_You/Domain/Entities/ScheduleList.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct ScheduleList {
+    let title: String
+    let location: String
+    let startTime: String
+    let scheduleSeq: Int
+    let feedGet: Bool //TODO: 추후에 서버에서 변수 추가해주면 변경 해야함
+}
