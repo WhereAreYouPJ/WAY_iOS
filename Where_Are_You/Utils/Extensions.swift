8fa722a5cc98db88c1d9160fae7d9ce82811c438
Juhee <viimv2000@gmail.com>
2025-02-17 16:21:04 +0900

    fix(#121): 1차 QA 버그사항 수정 - 일정 등록된게 없는 날에도 DailyScheduleView 모달창 띄우기

--- Where_Are_You/Utils/Extensions.swift
+++ Where_Are_You/Utils/Extensions.swift
@@ -377,11 +377,14 @@ extension DateFormatter {
 
 enum DateFormat: String {
     case server = "yyyy-MM-dd'T'HH:mm:ss.SSS"   // 서버의 날짜 형식
-    case serverSimple = "yyyy-MM-dd'T'HH:mm:ss"
+    case serverSimple = "yyyy-MM-dd'T'HH:mm:ss" // 서버의 날짜 형식
     case yearMonthDateHyphen = "yyyy-MM-dd"     // "yyyy-MM-dd" 형태
     case yearMonthDate = "yy.MM.dd"             // "YY.MM.dd" 형태
     case yearMonth = "yyyy.MM"                  // "YYYY.MM" 형태
     case monthDay = "MM월 dd일"                  // "MM월 dd일" 형태
+    case monthDaySimple = "M월 d일"              // "M월 d일" 형태
+    case time = "a h시 m분"                      // "오후 1시 1분" 형태
+    case timeSimple = "a h시"                   // "오후 1시" 형태
 }
 
 extension String {
@@ -420,6 +423,10 @@ extension Date {
         }
         
         return "방금 전"
+    }
+    
+    func determineTimeFormat() -> DateFormat {
+        return Calendar.current.component(.minute, from: self) == 0 ? .timeSimple : .time
     }
 }
 
