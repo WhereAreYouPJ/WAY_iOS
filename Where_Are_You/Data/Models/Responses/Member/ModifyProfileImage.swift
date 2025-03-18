d29594c4f8ceaabd90ec51fbb89d5967899009f3
jungseok-corine <nonehour@naver.com>
2025-03-18 11:46:22 +0900

    Feat(#35): put-ProfileImage
    프로필 이미지 변경 성공시 서버에서 URL값 리턴 받아 userDefaultManager에 추가하는 로직

---
+++ Where_Are_You/Data/Models/Responses/Member/ModifyProfileImage.swift
@@ -0,0 +1,14 @@
+//
+//  ModifyProfileImage.swift
+//  Where_Are_You
+//
+//  Created by 오정석 on 18/3/2025.
+//
+
+import Foundation
+
+struct ModifyProfileImage: Codable {
+    let status: Int
+    let message: String
+    let data: String
+}
